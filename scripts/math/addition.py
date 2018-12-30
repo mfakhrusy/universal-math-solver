b30d8a030127a780bc87b8fba8a131ee9b012b2f
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2018-12-30 14:08:09 +0700

    add initial python scripts for algorithm purpose

---
+++ scripts/math/addition.py
@@ -0,0 +1,9 @@
+# addition and substraction
+# basically any form of x + y - z
+
+def sumNumbers(numbers):
+  return sum(numbers)
+
+
+# test
+print(sumNumbers([1, 20, 3]))
---
+++ scripts/math/math.py
@@ -0,0 +1,7 @@
+# prototype for math implementation for any kind of problem
+# starting from the easiest one.
+
+def printme( str ):
+    "This prints a passed string into this function"
+    print (str)
+    return
